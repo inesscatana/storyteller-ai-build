fba30a5ef1fdc9fd329cdd692a4123d124736be6
inesscatana <ines.arts@hotmail.com>
2024-09-22 14:22:48 +0100

    Building the Home Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from 'next'
-import localFont from 'next/font/local'
 
 import './globals.css'
 
 import Header from '@/components/Header'
-
-const geistSans = localFont({
-	src: './fonts/GeistVF.woff',
-	variable: '--font-geist-sans',
-	weight: '100 900',
-})
-const geistMono = localFont({
-	src: './fonts/GeistMonoVF.woff',
-	variable: '--font-geist-mono',
-	weight: '100 900',
-})
 
 export const metadata: Metadata = {
 	title: 'StoryTeller AI',
@@ -28,9 +16,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body className="flex flex-col min-h-screen antialiased">
 				<Header />
 				{children}
 			</body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import Logo from '@/images/logo.png'
+import { Button } from '@/components/ui/button'
+import StoryWriter from '@/components/StoryWriter'
+
 export default function Home() {
 	return (
-		<main className="">
-			<h1>AI Agent GPTScript build</h1>
+		<main className="flex-1 flex flex-col">
+			<section className="flex-1 grid grid-cols-1 lg:grid-cols-2">
+				<div className="bg-purple-500 flex flex-col space-y-5 justify-center items-center order-1 lg:-order-1 pt-10 pb-10">
+					<Image src={Logo} height={250} alt="Logo" />
+					<Button asChild className="bg-purple-700 p-10 text-xl">
+						<Link href="/stories">Explore Story Library</Link>
+					</Button>
+				</div>
+
+				<StoryWriter />
+			</section>
 		</main>
 	)
 }
